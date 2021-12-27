630c89c61707fecf0fd0c33b7f79bde0b1340305
Patryk Sztuczka <patryksztuczka00@gmail.com>
2021-12-27 20:17:03 +0100

    Added functionalities of updating, removing and liking

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import * as actionTypes from '../constants/actionTypes';
 import * as api from '../api';
 
 //action creator - functions that return actions
@@ -5,18 +6,45 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        dispatch({ type: "FETCH_ALL", payload: data });
+        dispatch({ type: actionTypes.FETCH_ALL, payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 };
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-        dispatch({ type: "CREATE", payload: data });
+        dispatch({ type: actionTypes.CREATE, payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
+    }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+        dispatch({ type: actionTypes.UPDATE, payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+        dispatch({ type: actionTypes.DELETE, payload: id });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+        dispatch({ type: actionTypes.LIKE, payload: data });
+    } catch (error) {
+        console.log(error);
     }
 };
 
